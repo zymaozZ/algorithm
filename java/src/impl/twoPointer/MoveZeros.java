1c9ac399621ae863662e739027da18e26d435283
zhengyu.mao <zhengyu.mao@yintech.cn>
2021-03-11 00:05:39 +0800

    practise one more time

--- java/src/impl/twoPointer/MoveZeros.java
+++ java/src/impl/twoPointer/MoveZeros.java
@@ -30,7 +30,34 @@ public class MoveZeros {
         }
     }
 
+    public void moveZerosBak1(int[] nums) {
+        int j = 0;
+        for (int i = 0;i < nums.length;i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        for (int i = j;i < nums.length;i++) {
+            nums[i] = 0;
+        }
+    }
+
     public void moveZerosV2(int[] nums) {
+        int j = 0;
+        for (int i = 0;i < nums.length;i++) {
+            if (nums[i] != 0) {
+                if (i > j) {
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+
+    public void moveZerosV2Bak(int[] nums) {
         int j = 0;
         for (int i = 0;i < nums.length;i++) {
             if (nums[i] != 0) {
